61f54de10cdee80ac7e9dc666461d1de29c221be
grangerquery <braydengranger04@gmail.com>
2026-02-09 14:45:19 -0500

    02-09-2026

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,19 +1,19 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		opts = {
-			flavour = "mocha",
-			no_italic = true,
-			auto_integrations = true,
-		},
-		config = function(_, opts)
-			require("catppuccin").setup(opts)
-			--vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+	--	{
+	--		"catppuccin/nvim",
+	--		lazy = false,
+	--		name = "catppuccin",
+	--		priority = 1000,
+	--		opts = {
+	--			flavour = "mocha",
+	--			no_italic = true,
+	--			auto_integrations = true,
+	--		},
+	--		config = function(_, opts)
+	--			require("catppuccin").setup(opts)
+	--vim.cmd.colorscheme("catppuccin")
+	--		end,
+	--	},
 	{
 		"bjarneo/pixel.nvim",
 		priority = 1000,
